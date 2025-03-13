94d2c7d11d31e615bda38d037859925af3aac14d
R0flan4k <klexak05@gmail.com>
2025-03-13 03:34:36 +0300

    Wrong CMD input handling.

--- ParaCL/src/paracl.cpp
+++ ParaCL/src/paracl.cpp
@@ -12,15 +12,30 @@ int main(int argc, char **argv)
         std::cerr << "Input file expected.\n";
         return 1;
     }
-    std::ifstream stream(argv[1]);
+    std::string ifile_name(argv[1]);
+    std::ifstream stream(ifile_name);
+    if (stream.fail())
+    {
+        std::cerr << "File " << ifile_name << " is not exhisting.\n";
+        return 1;
+    }
 
-    std::unique_ptr<FlexLexer> lexer = std::make_unique<yyFlexLexer>(&stream);
-    yy::DriverPCL driver(lexer.release());
-    yy::ast_representation_t astr;
-    driver.parse(&astr);
+    try
+    {
+        std::unique_ptr<FlexLexer> lexer = std::make_unique<yyFlexLexer>(&stream);
+        yy::DriverPCL driver(lexer.release());
+        yy::ast_representation_t astr;
+        driver.parse(&astr);
 
-    std::ofstream asts("./AST_dump"), sts("./ST_dump");
-    yy::astr_dumper dumper(&asts, &sts); dumper(astr);
-    astr.execute();
+        std::ofstream asts("./AST_dump"), sts("./ST_dump");
+        yy::astr_dumper dumper(&asts, &sts); dumper(astr);
+        astr.execute();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
+    
     return 0;
 }
