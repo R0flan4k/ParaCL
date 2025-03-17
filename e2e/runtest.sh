5262f7318615f186bcba90e7abeb8dda81754d8b
Ruslan Alekseyevich <142314284+R0flan4k@users.noreply.github.com>
2025-03-17 23:11:03 +0300

    Update runtest.sh

--- e2e/runtest.sh
+++ e2e/runtest.sh
@@ -10,9 +10,9 @@ eval ${TESTER} ${TEST} < $TESTDAT > $NAME.log
 DIFF=$(diff -w $NAME.log ${ANS})
 
 if [ $? -ne 0 ]; then
-  echo "" >> $NAME.log
-  echo "Diff is:" >> $NAME.log
-  echo "$DIFF" >> $NAME.log
+  echo "" # >> $NAME.log
+  echo "Diff is:" # >> $NAME.log
+  echo "$DIFF" # >> $NAME.log
   echo "Test ${NAME} failed, see ${NAME}.log"
   exit 1
 else
